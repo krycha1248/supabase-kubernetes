12545741539350ed49fd5a9d46d6dcce636a41df
Krystian Włodek <krycha1248@gmail.com>
2026-05-21 10:45:32 +0200

    changed deps and variables

--- dns.tf
+++ dns.tf
@@ -1,5 +1,5 @@
 resource "cloudflare_dns_record" "A" {
-  name    = kubernetes_namespace_v1.supabase.metadata[0].name
+  name    = var.domain
   zone_id = var.cloudflare_zone_id
   type    = "A"
   ttl     = 1
